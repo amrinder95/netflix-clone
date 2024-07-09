5308d5d95f17ce1ee7b6925dc5f5a09cdde20f27
Amrinder Singh <amrinder995@gmail.com>
2024-07-08 20:46:44 -0400

    populate PlansScreen with subscription options, implement stripe checkout

--- src/screens/PlansScreen.js
+++ src/screens/PlansScreen.js
@@ -1,9 +1,13 @@
 import React, { useEffect, useState } from 'react'
 import './PlansScreen.css';
 import db from '../firebase';
-
+import { useSelector } from 'react-redux';
+import { selectUser } from '../features/userSlice';
+import { loadStripe } from '@stripe/stripe-js';
+ 
 function PlansScreen() {
   const [products, setProducts] = useState([]);
+  const user = useSelector(selectUser);
 
   useEffect(() => {
     db.collection('products').where('active','==', true)
@@ -26,8 +30,42 @@ function PlansScreen() {
 
   console.log(products);
 
+  const loadCheckout = async (priceId) => {
+    const docRef = await db.collection('customers')
+      .doc(user.uid)
+      .collection('checkout_sessions')
+      .add({
+        price: priceId,
+        success_url: window.location.origin,
+        cancel_url: window.location.origin,
+      });
+    
+    docRef.onSnapshot(async(snap) => {
+      const { error, sessionId } = snap.data();
+      if (error) {
+        alert(`An error occured: ${error.message}`);
+      }
+      if (sessionId) {
+        const stripe = await loadStripe('pk_test_51PaJSPKlZviF2iNIHmie4uQvBBpUUMReHMGvATLHXqBw3qzDlgG85iyw13MFe2XRQm9bD6lNY3MHbJ9QH4SssEwu00SjNJn04b')
+        stripe.redirectToCheckout({ sessionId });
+      }
+      
+    })
+  }
+
   return (
     <div className='plansScreen'>
+    {Object.entries(products).map(([productId, productData]) => {
+      return (
+        <div className="plansScreen__plan">
+          <div className="plansScreen_info">
+            <h5>{productData.name}</h5>
+            <h6>{productData.description}</h6>
+          </div>
+          <button onClick={() => loadCheckout(productData.prices.priceId)}>Subscribe</button>
+        </div>
+      )
+    })}
 
     </div>
   )
